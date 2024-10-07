11892087178cb9051894c4b63b2753f09abef1c7
Sergio González <gonzalez.coser24@triana.salesianos.edu>
2024-10-07 14:27:11 +0200

    First commit

---
+++ src/main/java/com/salesianostriana/dam/colegio/service/AlumnoService.java
@@ -0,0 +1,12 @@
+package com.salesianostriana.dam.colegio.service;
+
+import com.salesianostriana.dam.colegio.model.Alumno;
+import com.salesianostriana.dam.colegio.repository.AlumnoRepository;
+import com.salesianostriana.dam.colegio.service.base.BaseServiceImpl;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AlumnoService extends BaseServiceImpl<Alumno, Long, AlumnoRepository> {
+
+
+}
